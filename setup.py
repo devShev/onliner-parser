83a84fb882706aca4ba2279db9e27c189f7b1594
Андрей Альшевский <aalshe38@gmail.com>
2022-10-19 18:04:48 +0300

    Fix pypi settings

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
-    "pydantic==1.10.2",
-    "requests==2.28.1",
-    "progress==1.6",
-    "pandas==1.5.0",
-    "openpyxl==3.0.10",
-    "beautifulsoup4==4.11.1",
+    "pydantic",
+    "requests",
+    "progress",
+    "pandas",
+    "openpyxl",
+    "beautifulsoup4",
 ]
 
 setup(
     name='onliner_parser',
-    version='0.2.1',
+    version='0.2.1.1',
     description='Парсер для каталога Onliner.by',
-    packages=['onliner_parser'],
+    packages=find_packages(),
     install_requires=requirements,
     author_email='aalshe38@gmail.com',
     zip_safe=False,
